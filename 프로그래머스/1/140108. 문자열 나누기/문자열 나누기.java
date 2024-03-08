af990332be376f965695f4b175aedddd1af36a7c
BinJinu <138997296+BinJinu@users.noreply.github.com>
2024-03-08 20:10:05 +0900

    [level 1] Title: 문자열 나누기, Time: 0.91 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 1;
+        int count =1;
+        char ch = s.charAt(0);
+        
+        for(int i=1;i<s.length();i++){
+            if(count==0){
+                answer++;
+                ch = s.charAt(i);
+            }
+            if(ch==s.charAt(i)){
+                count++;
+            }
+            else count--;
+        }
+        
+        return answer;
+    }
+}
